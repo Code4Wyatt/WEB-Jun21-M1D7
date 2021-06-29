d8089389f5dc2ce6520996cc90c84d50573c0367
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-06-29 14:54:40 +0100

    Completed Ex 8 and 9

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ changedH.innerText = "Strive Phone - The Best!"
 
 let changeBackground = document.getElementsByTagName('body')
 changeBackground = document.body.style.backgroundColor = "#EEF5FA";
+
+document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].innerHTML = "Strive School" + " Located at 123 Fake Street";
