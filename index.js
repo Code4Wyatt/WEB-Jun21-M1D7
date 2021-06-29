1a456f22656ec71a08f507c70dfc141c33644eda
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-06-29 14:45:27 +0100

    Completed Ex 2

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 let changedH = document.getElementById('h1')
 changedH.innerText = "Strive Phone - The Best!"
+
+let changeBackground = document.getElementsByTagName('body')
+changeBackground = document.body.style.backgroundColor = "#EEF5FA";
